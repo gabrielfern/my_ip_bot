7cf1536a40262cd71f3c9068bd5a3b84e9ef8bb4
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-09-28 02:44:02 -0300

    setting things up

---
+++ httpbin.js
@@ -0,0 +1,24 @@
+const http = require('http'),
+      Event = require('events')
+
+class HttpBin extends Event {
+    constructor() {
+        super()
+        this.url = 'http://httpbin.org/'
+    }
+    getIp() {
+        http.get(this.url + 'ip', (res) => {
+            res.on('data', (data) => {
+                this.emit('ip', JSON.parse(data.toString()).origin)
+            })
+        })
+    }
+}
+
+module.exports = HttpBin
+
+if (require.main == module) {
+    hb = new HttpBin()
+    hb.on('ip', ip => console.log(ip))
+    hb.getIp()
+}
