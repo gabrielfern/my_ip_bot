a0f5ce299bbd9302acf3c5ebcecc50eff8e1f99c
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-09-28 16:08:41 -0300

    linking

--- my_ip.js
+++ my_ip.js
@@ -4,37 +4,40 @@ const https = require('https'),
       info = JSON.parse(fs.readFileSync('personal_info.json').toString()),
       url = `https://api.telegram.org/bot${info.token}/`
 
-//https.get(url + 'getMe', resp => {
+// https.get(url + 'getMe', resp => {
 //    resp.on('data', data => {
 //        console.log(JSON.parse(data.toString()))
 //    })
-//})
-
-//https.get(url + 'getUpdates', resp => {
+// })
+//
+// https.get(url + 'getUpdates', resp => {
 //    resp.on('data', data => {
 //        let results = JSON.parse(data.toString()).result
 //        console.log('Length: ' + results.length)
 //        console.log(results[9])
 //    })
-//})
+// })
 
-let body = {
-        chat_id: `${some_chat}`,
-        text: 'hello, what you want of me?'
-    },
-    stringifyed = JSON.stringify(body),
-    options = {
-        hostname: 'api.telegram.org',
-        port: 443,
-        path: `/bot${info.token}/sendMessage`,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(stringifyed)
-        }
-    },
-    req = https.request(options, resp => {
-        resp.on('data', data => console.log(data.toString()))
-    })
+httpbin.on('ip', ip => {
+    let body = {
+            chat_id: 313287164,
+            text: ip
+        },
+        stringifyed = JSON.stringify(body),
+        options = {
+            hostname: 'api.telegram.org',
+            port: 443,
+            path: `/bot${info.token}/sendMessage`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(stringifyed)
+            }
+        },
+        req = https.request(options, resp => {
+            resp.on('data', data => console.log(data.toString()))
+        })
+    req.end(stringifyed)
+})
 
-req.end(stringifyed)
+httpbin.getIp()
