9c1a769dfe1aa02b7d8822af65bce3220e9b8d82
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-09-29 00:26:47 -0300

    unclear

--- my_ip.js
+++ my_ip.js
@@ -5,11 +5,9 @@ const https = require('https'),
       info = JSON.parse(fs.readFileSync('personal_info.json').toString()),
       url = `https://api.telegram.org/bot${info.token}/`
 
-function getUpdates(offset) {
-    if (offset == undefined)
-        offset = info.offset
+function getUpdates() {
     let body = {
-            offset: info.update_id + offset,
+            offset: info.update_id + info.offset,
             timeout: 120
         },
         stringifyed = JSON.stringify(body),
@@ -26,7 +24,25 @@ function getUpdates(offset) {
         req = https.request(options)
     req.on('response', resp => {
         resp.on('data', data => {
-            console.log(JSON.parse(data.toString()))
+            data = JSON.parse(data.toString())
+            if (data.ok) {
+                info.offset += data.result.length
+                for (update of data.result) {
+                    console.log('=====')
+                    console.log('update:', update.update_id)
+                    console.log('username:', update.message.chat.username)
+                    console.log('chat:', update.message.chat.id)
+                    console.log('text:', update.message.text)
+                    if (update.message.text == '/ip')
+                        if (update.message.chat.username == 'gabrielfernndss')
+                            sendIp(update.message.chat.id)
+                        else
+                            sendMessage(update.message.chat.id,
+                                'Sorry, u\'re not Gabriel')
+                }
+                flushJSON('personal_info.json', info)
+            }
+            getUpdates()
         })
     })
     req.on('error', err => console.log(err))
@@ -53,15 +69,22 @@ function sendMessage(chatId, text) {
     req.on('response', resp => {
         resp.on('data', data => {
             console.log(JSON.parse(data.toString()).ok)
+            console.log(Date.now())
         })
     })
     req.on('error', err => console.log(err))
     req.end(stringifyed)
 }
 
-httpbin.on('ip', ip => {
-    sendMessage(info.chat_id, ip)
-})
-httpbin.getIp()
+function sendIp(chatId) {
+    httpbin.on('ip', ip => {
+        sendMessage(chatId, ip)
+    })
+    httpbin.getIp()
+}
 
-// getUpdates()
+function flushJSON(file, obj) {
+    fs.writeFileSync(file, JSON.stringify(obj))
+}
+
+getUpdates()
