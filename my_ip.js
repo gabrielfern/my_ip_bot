5ff49946b0b865a54ee83df98a7b8c33f25d065d
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-09-28 22:45:09 -0300

    functions wrapping

--- my_ip.js
+++ my_ip.js
@@ -1,27 +1,42 @@
 const https = require('https'),
+      http = require('http'),
       fs = require('fs'),
       httpbin = new (require('./httpbin'))(),
       info = JSON.parse(fs.readFileSync('personal_info.json').toString()),
       url = `https://api.telegram.org/bot${info.token}/`
 
-// https.get(url + 'getMe', resp => {
-//    resp.on('data', data => {
-//        console.log(JSON.parse(data.toString()))
-//    })
-// })
-
-// https.get(url + 'getUpdates', resp => {
-//    resp.on('data', data => {
-//        let result = JSON.parse(data.toString())
-//
-//        console.log(result['result'][12])
-//    })
-// })
-
-httpbin.on('ip', ip => {
+function getUpdates(offset) {
+    if (offset == undefined)
+        offset = info.offset
     let body = {
-            chat_id: info.chat_id,
-            text: ip
+            offset: info.update_id + offset,
+            timeout: 120
+        },
+        stringifyed = JSON.stringify(body),
+        options = {
+            hostname: 'api.telegram.org',
+            port: 443,
+            path: `/bot${info.token}/getUpdates`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(stringifyed)
+            }
+        },
+        req = https.request(options)
+    req.on('response', resp => {
+        resp.on('data', data => {
+            console.log(JSON.parse(data.toString()))
+        })
+    })
+    req.on('error', err => console.log(err))
+    req.end(stringifyed)
+}
+
+function sendMessage(chatId, text) {
+    let body = {
+            "chat_id": chatId,
+            "text": text
         },
         stringifyed = JSON.stringify(body),
         options = {
@@ -34,9 +49,19 @@ httpbin.on('ip', ip => {
                 'Content-Length': Buffer.byteLength(stringifyed)
             }
         },
-        req = https.request(options, resp => {
-            resp.on('data', data => console.log(data.toString()))
+        req = https.request(options)
+    req.on('response', resp => {
+        resp.on('data', data => {
+            console.log(JSON.parse(data.toString()).ok)
         })
+    })
+    req.on('error', err => console.log(err))
     req.end(stringifyed)
+}
+
+httpbin.on('ip', ip => {
+    sendMessage(info.chat_id, ip)
 })
 httpbin.getIp()
+
+// getUpdates()
