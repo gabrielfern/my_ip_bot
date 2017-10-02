ccd4b912a6ec8bfb50e3c03234a56a1625a01b76
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-10-02 13:12:53 -0300

    readline

--- setup.js
+++ setup.js
@@ -1,10 +1,23 @@
-const readline = require('readline'),
+const fs = require('fs'),
+      readline = require('readline'),
       rl = readline.createInterface({
           input: process.stdin,
           output: process.stdout
       })
 
-rl.question('Provide your telegram bot api token\n--> ', resp => {
-    rl.write('done\n')
-    rl.close()
+rl.question('Provide your telegram bot api token\n--> ',
+token => {
+    rl.question('your personal telegram username(without @)\n--> ',
+    username => {
+        rl.question('your message to make the bot send back the ip\n--> ',
+        message => {
+            let info = {
+                'token': token,
+                'me': username,
+                'command': message
+            }
+            console.log(info)
+            rl.close()
+        })
+    })
 })
