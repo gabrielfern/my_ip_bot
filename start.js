41b764304655d2b2ab588c67ed2f1963fd12ade7
Gabriel Fernandes <gabrielfernndss@gmail.com>
2017-10-02 19:13:12 -0300

    fresh

--- start.js
+++ start.js
@@ -5,10 +5,13 @@ const https = require('https'),
 
 function getUpdates() {
     let body = {
-            'offset': info.update_id + info.offset,
             'timeout': 120
         },
-        stringifyed = JSON.stringify(body),
+        // fresh refer to whether we have here a new bot or not
+        fresh = info.update_id == undefined || info.offset == undefined
+    if (!fresh)
+        body.offset = info.update_id + info.offset
+    let stringifyed = JSON.stringify(body),
         options = {
             'hostname': 'api.telegram.org',
             'port': 443,
@@ -24,30 +27,36 @@ function getUpdates() {
         resp.on('data', data => {
             data = JSON.parse(data.toString())
             if (data.ok) {
-                // set received messages as "read"
-                // since the offset function is to tell telegram bot api
-                // where to start of next update
-                info.offset += data.result.length
-                for (update of data.result) {
-                    // log interesting things about the messages
-                    console.log('=====')
-                    console.log('update:', update.update_id)
-                    console.log('username:', update.message.chat.username)
-                    console.log('chat:', update.message.chat.id)
-                    console.log('text:', update.message.text)
-                    // if message was request for ip and from you
-                    // send ip address
-                    if (update.message.text == info.command)
-                        if (update.message.chat.username == info.me)
-                            sendIp(update.message.chat.id)
+                if (data.result.length > 0) {
+                    if (fresh) {
+                        info.update_id = data.result[0]
+                        info.offset = 0
+                    }
+                    // set received messages as "read"
+                    // since the offset function is to tell telegram bot api
+                    // where to start of next update
+                    info.offset += data.result.length
+                    for (update of data.result) {
+                        // log interesting things about the messages
+                        console.log('=====')
+                        console.log('update:', update.update_id)
+                        console.log('username:', update.message.chat.username)
+                        console.log('chat:', update.message.chat.id)
+                        console.log('text:', update.message.text)
+                        // if message was request for ip and from you
+                        // send ip address
+                        if (update.message.text == info.command)
+                            if (update.message.chat.username == info.me)
+                                sendIp(update.message.chat.id)
+                            else
+                                sendMessage(update.message.chat.id,
+                                    `sorry, u're not @${info.me}`)
                         else
                             sendMessage(update.message.chat.id,
-                                `sorry, u're not @${info.me}`)
-                    else
-                        sendMessage(update.message.chat.id,
-                            'sorry, not a ip request')
+                                'sorry, not a ip request')
+                    }
+                    flushJSON('personal_info.json', info)
                 }
-                flushJSON('personal_info.json', info)
             }
             // a trick to escape of recursion
             setImmediate(getUpdates)
